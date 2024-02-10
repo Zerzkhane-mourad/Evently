d06bf5669469f4f6071d41d54cf1e8203a978727
zerzkhane <zerzkhanemourad@gmail.com>
2024-02-10 02:48:10 +0100

    add MongoDbConnection

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -24,7 +24,17 @@ export default function Home() {
             height={1000}
             className="max-h-[70vh] object-contain object-center 2xl:max-h-[50vh]"
           />
+        </div>
+      </section>
 
+      <section id="events" className="wrapper my-8 flex flex-col gap-8 md:gap-12" >
+        <h2 className="h2-bold">
+          Trust by <br /> Thousands of events
+        </h2>
+
+        <div className="flex w-full flex-col gap-5 md:flex-row">
+          Search
+          CategoryFilter
         </div>
       </section>
     </main>
