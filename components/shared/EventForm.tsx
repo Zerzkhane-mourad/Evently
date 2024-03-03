46d6b68c58e523f4aa231fed80951c51d2022582
zerzkhane <zerzkhanemourad@gmail.com>
2024-03-03 23:50:24 +0100

    add dropdown in event form

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -14,7 +14,13 @@ import {
     FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
+import { Textarea } from "@/components/ui/textarea"
 import { eventFormSchema } from "@/lib/validator"
+import { eventDefaultValues } from "@/constants"
+import Dropdown from "./Dropdown"
+import FileUploader from "./FileUploader"
+import { useState } from "react"
+
 
 
 type EventFormProps = {
@@ -26,11 +32,13 @@ type EventFormProps = {
 
 const EventForm = ({ userId, type }: EventFormProps) => {
 
+    const initialValues = eventDefaultValues;
+    const [files, setFiles] = useState<File[]>([])
+
+
     const form = useForm<z.infer<typeof eventFormSchema>>({
         resolver: zodResolver(eventFormSchema),
-        defaultValues: {
-            username: "",
-        },
+        defaultValues: initialValues
     })
 
     // 2. Define a submit handler.
@@ -42,23 +50,63 @@ const EventForm = ({ userId, type }: EventFormProps) => {
 
     return (
         <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-                <FormField
-                    control={form.control}
-                    name="username"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>Username</FormLabel>
-                            <FormControl>
-                                <Input placeholder="shadcn" {...field} />
-                            </FormControl>
-                            <FormDescription>
-                                This is your public display name.
-                            </FormDescription>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
+            <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-5">
+                <div className="flex flex-col gap-5 md:flex-row">
+                    <FormField
+                        control={form.control}
+                        name="title"
+                        render={({ field }) => (
+                            <FormItem className="w-full" >
+                                <FormControl>
+                                    <Input placeholder="Event Title" className="input-field" {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="categoryId"
+                        render={({ field }) => (
+                            <FormItem className="w-full" >
+                                <FormControl>
+                                    <Dropdown onChangeHandler={field.onChange} value={field?.value} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                </div>
+
+                <div className="flex flex-col gap-5 md:flex-row ">
+                    <FormField
+                        control={form.control}
+                        name="description"
+                        render={({ field }) => (
+                            <FormItem className="w-full" >
+                                <FormControl className="h-72">
+                                    <Textarea placeholder="Description" {...field} className="textarea rounded-2    xl" />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+
+                    <FormField
+                        control={form.control}
+                        name="imageUrl"
+                        render={({ field }) => (
+                            <FormItem className="w-full" >
+                                <FormControl className="h-72">
+                                    <FileUploader />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                </div>
+
+
                 <Button type="submit">Submit</Button>
             </form>
         </Form>)
